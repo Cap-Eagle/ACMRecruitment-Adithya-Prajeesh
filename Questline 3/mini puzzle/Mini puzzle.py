76cc01683ecb9d6431627d838b7a357c027c6543
Cap-Eagle <adithyaprajeesh23@gmail.com>
2025-09-13 22:16:02 +0530

    Update Mini puzzle.py

--- Questline 3/mini puzzle/Mini puzzle.py
+++ Questline 3/mini puzzle/Mini puzzle.py
@@ -3,9 +3,10 @@ def rotate_and_convert(binary_str, k):
     rotated = binary_str[-k:] + binary_str[:-k]
     decimal_value = int(rotated, 2)
     return decimal_value
-binary_str = input('enter binary string')
+binary_str = input('enter binary string:')
 k = 2
 result = rotate_and_convert(binary_str, k) 
 print('original:',binary_str)
 print('rotated:',binary_str[-k:] + binary_str[:-k])
 print('decimal value:', result)
+
